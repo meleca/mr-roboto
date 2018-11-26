9f82b87261b299ada2a2bb9ac8b546651acf6a6c
Juliano Fernandes <julianofernandes@gmail.com>
2018-11-26 19:34:07 -0200

    Adjusts coding style (#32)
    * normalizes comments and docstrings.
    
    * single quotes for strings and double quotes for docstrings
    
    * single method for string interpolation

--- migrations/env.py
+++ migrations/env.py
@@ -3,24 +3,9 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline():
@@ -33,9 +18,8 @@ def run_migrations_offline():
 
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option('sqlalchemy.url')
     context.configure(
         url=url, target_metadata=target_metadata, literal_binds=True)
 
@@ -48,7 +32,6 @@ def run_migrations_online():
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
--- mr_roboto.py
+++ mr_roboto.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from irc3 import IrcBot, utils
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -7,28 +6,39 @@ import os
 
 
 class ReloadEventHandler(FileSystemEventHandler):
+    """Handles auto reload whenever a plugin source code changes."""
     def __init__(self, bot, config):
+        """Initializes the handler with the following arguments.
+
+        Args:
+            bot: The IrcBot instance that must be reloaded.
+            config: The configuration set used to initialize the bot.
+        """
         self.bot = bot
         self.config = config
 
     def on_modified(self, event):
+        """Called whenever a file or directory is modified.
+
+        Args:
+            event: Event representing the modified file or directory.
+        """
         super(ReloadEventHandler, self).on_modified(event)
 
-        if (event.src_path.endswith(".py") and
-                os.path.dirname(event.src_path).endswith("plugins")):
-            local_plugins = filter(lambda lp: lp.startswith("plugins"),
-                                   self.config["includes"])
+        if (event.src_path.endswith('.py') and
+                os.path.dirname(event.src_path).endswith('plugins')):
+            local_plugins = filter(lambda lp: lp.startswith('plugins'),
+                                   self.config['includes'])
             self.bot.reload(*local_plugins)
 
 
 def main():
-    # parse configs
+    """Initializes a new irc3 bot."""
     if len(sys.argv) != 2:
         print('Usage: mr_roboto <settings_file>')
         sys.exit(1)
     config_path = os.path.abspath(sys.argv[1])
     config = utils.parse_config('bot', config_path)
-    # start bot
     bot = IrcBot.from_config(config)
     observer = Observer()
     observer.schedule(ReloadEventHandler(bot, config),
