02dea1a13b9136a5f7f8a1112e7273d580d193bf
Juliano Fernandes <julianofernandes@gmail.com>
2018-12-01 11:47:24 -0200

    add test for database plugin

---
+++ test/test_database.py
@@ -0,0 +1,20 @@
+from unittest import mock
+from plugins.database import Database
+
+
+@mock.patch('plugins.database.dataset')
+def test_database_connection(mock_dataset):
+    """Tests the initialization of Database plugin.
+
+    It creates a database connection using dataset library.
+
+    Args:
+        mock_dataset: Fakes dataset libraty.
+    """
+    bot = mock.Mock()
+    bot.config = mock.Mock(database='database-url')
+    con = mock.Mock()
+    mock_dataset.connect.return_value = con
+    Database(bot)
+    assert mock_dataset.connect.called_once_with(bot.config.database)
+    assert bot.dataset == con
