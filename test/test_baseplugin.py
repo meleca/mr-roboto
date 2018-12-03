8fab165aeb4950f87c6ef76459ab8b15b87616f3
Juliano Fernandes <julianofernandes@gmail.com>
2018-12-03 17:58:38 -0200

    applies bot's fixture to the existing tests (#46)

--- test/test_baseplugin.py
+++ test/test_baseplugin.py
@@ -1,10 +1,12 @@
-from unittest import mock
 from plugins.baseplugin import BasePlugin
 
 
-def test_BasePlugin_reload():
-    """Tests BasePlugin reload method."""
-    bot = mock.Mock()
+def test_BasePlugin_reload(bot):
+    """Tests BasePlugin reload method.
+
+    Args:
+        bot: Fake instance of an Irc3Bot.
+    """
     old = BasePlugin(bot)
     new = BasePlugin.reload(old)
     assert isinstance(new, BasePlugin)
--- test/test_mr_roboto.py
+++ test/test_mr_roboto.py
@@ -6,7 +6,7 @@ from unittest import mock
 @mock.patch('mr_roboto.ReloadEventHandler')
 @mock.patch('mr_roboto.Observer')
 @mock.patch('mr_roboto.IrcBot')
-def test_main(mock_ircbot, mock_observer, mock_reloader, root_directory):
+def test_main(mock_ircbot, mock_observer, mock_reloader, root_directory, bot):
     """Tests bot regular initialization.
 
     Args:
@@ -14,28 +14,28 @@ def test_main(mock_ircbot, mock_observer, mock_reloader, root_directory):
         mock_observer: Fakes the Observer from watchdog library.
         mock_reloader: Fakes the ReloadEventHandler class.
         root_directory: Path to the application's root directory.
+        bot: Fake instance of an Irc3Bot.
     """
-    ircbot = mock.Mock()
     observer = mock.Mock()
     reloader = mock.Mock()
-    mock_ircbot.from_config.return_value = ircbot
+    mock_ircbot.from_config.return_value = bot
     mock_observer.return_value = observer
     mock_reloader.return_value = reloader
     mr_roboto.main()
     assert mock_ircbot.from_config.called_once_with(mr_roboto.conf)
-    assert mock_reloader.called_once_with(ircbot, mr_roboto.conf)
+    assert mock_reloader.called_once_with(bot, mr_roboto.conf)
     assert mock_observer.called_once
     assert observer.schedule.called_once_with(
         reloader, root_directory, recursive=True)
     assert observer.start.called_once
-    assert ircbot.run.called_once_with(forever=True)
+    assert bot.run.called_once_with(forever=True)
 
 
 @mock.patch('mr_roboto.ReloadEventHandler')
 @mock.patch('mr_roboto.Observer')
 @mock.patch('mr_roboto.IrcBot')
 def test_main_kbinterrupt(
-    mock_ircbot, mock_observer, mock_reloader, root_directory
+    mock_ircbot, mock_observer, mock_reloader, root_directory, bot
 ):
     """Tests keyboard interruption after bot's initialization.
 
@@ -44,32 +44,32 @@ def test_main_kbinterrupt(
         mock_observer: Fakes the Observer from watchdog library.
         mock_reloader: Fakes the ReloadEventHandler class.
         root_directory: Path to the application's root directory.
+        bot: Fake instance of an Irc3Bot.
     """
-    ircbot = mock.Mock()
-    ircbot.run.side_effect = KeyboardInterrupt()
+    bot.run.side_effect = KeyboardInterrupt()
     observer = mock.Mock()
     reloader = mock.Mock()
-    mock_ircbot.from_config.return_value = ircbot
+    mock_ircbot.from_config.return_value = bot
     mock_observer.return_value = observer
     mock_reloader.return_value = reloader
     mr_roboto.main()
     assert mock_ircbot.from_config.called_once_with(mr_roboto.conf)
-    assert mock_reloader.called_once_with(ircbot, mr_roboto.conf)
+    assert mock_reloader.called_once_with(bot, mr_roboto.conf)
     assert mock_observer.called_once
     assert observer.schedule.called_once_with(
         reloader, root_directory, recursive=True)
     assert observer.start.called_once
-    assert ircbot.run.called_once_with(forever=True)
+    assert bot.run.called_once_with(forever=True)
     assert observer.stop.called_once
 
 
-def test_ReloadEventHandler_on_modified(root_directory):
+def test_ReloadEventHandler_on_modified(root_directory, bot):
     """Tests the app reload handler for source code changes.
 
     Args:
         root_directory: Path to the application's root directory.
+        bot: Fake instance of an Irc3Bot.
     """
-    bot = mock.Mock()
     event = mock.Mock()
     event.src_path = path.join(root_directory, 'plugins', 'behaviors.py')
     reloader = mr_roboto.ReloadEventHandler(bot, mr_roboto.conf)
@@ -77,14 +77,14 @@ def test_ReloadEventHandler_on_modified(root_directory):
     assert bot.reload.called_once
 
 
-def test_ReloadEventHandler_on_modified_untracked(root_directory):
+def test_ReloadEventHandler_on_modified_untracked(root_directory, bot):
     """Tests the app reload handler when the changed
     file is not on the tracking list.
 
     Args:
         root_directory: Path to the application's root directory.
+        bot: Fake instance of an Irc3Bot.
     """
-    bot = mock.Mock()
     event = mock.Mock()
     event.src_path = path.join(root_directory, 'alembic.ini')
     reloader = mr_roboto.ReloadEventHandler(bot, mr_roboto.conf)
