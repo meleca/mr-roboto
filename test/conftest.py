add949c55fd647a09ecf24803449db2b225b3c53
Juliano Fernandes <julianofernandes@gmail.com>
2018-12-07 17:55:44 -0200

    add tests for commands plugin (#51)

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from os import path
 from unittest import mock
+from datetime import datetime
 
 
 @pytest.fixture
@@ -16,9 +17,24 @@ def bot():
     mock_bot.nick = 'mybot'
     mock_bot.config = mock.Mock(database='sqlite:///:memory:')
     mock_bot.channels = ['#meleca']
+
+    # A few database mock workarounds.
+    class MockDatetime(datetime):
+        def __neg__(self):
+            return self
     tb_greetings = mock.Mock()
-    tb_url_history = mock.Mock()
     tb_greetings.find_one.return_value = {'options': 'Hey there!'}
+    tb_url_history = mock.Mock()
+    tb_url_history.table.c.datetime = MockDatetime.utcnow()
     database = {'greetings': tb_greetings, 'url_history': tb_url_history}
-    mock_bot.dataset = database
+    mock_bot.dataset = mock.MagicMock()
+    mock_bot.dataset.query.return_value = [{
+        'title': 'mr-roboto',
+        'url': 'https://github.com/meleca/mr-roboto',
+        'datetime': datetime.fromisoformat('2018-12-06')
+    }]
+    mock_bot.dataset.__contains__.side_effect = database.__contains__
+    mock_bot.dataset.__getitem__.side_effect = database.__getitem__
+    mock_bot.dataset.__iter__.side_effect = database.__iter__
+
     return mock_bot
